09a3156a079f9f2790218d4e89238383c67d6943
datawolf <wanglong@cse.buaa.edu.cn>
2013-09-21 09:23:17 -0400

    modified the list_all_file_system_type

--- 005_list_all_file_system_type/list_all_file_system_type.c
+++ 005_list_all_file_system_type/list_all_file_system_type.c
@@ -11,40 +11,17 @@
 
 static int __init list_all_file_system_type_init(void)
 {
-	char *sym_name = "file_systems";
-    	unsigned long sym_addr = kallsyms_lookup_name(sym_name);
 	struct file_system_type **tmp;
-//	"file_systems"  address :ffffffff8201f928
-	struct file_system_type *fst = (struct file_system_type *) sym_addr ;
-//	const char * symbol_name;
-//	void *addr;
-//	symbol_name = "__symbol_get";
-//	addr = __symbol_get(symbol_name);
-
-/*	
-	if (addr != NULL)	
-		printk(KERN_ALERT "the address of file_systems is: %lx\n", (unsigned long)addr);
-	else
-		printk(KERN_ALERT "is not found");
-*/
-	tmp = &fst;
+//	"file_systems"  address 
+//	ffffffff81acae20
+	struct file_system_type *fs = (struct file_system_type *)0xffffffff81acae20;
+	tmp = &fs;
 	while (*tmp){
 		printk(KERN_ALERT "file system type : %s\n", (*tmp)->name);
 		tmp = &(*tmp)->next;
 	}
 	
-	printk(KERN_ALERT "*****************************************\n");
-	printk(KERN_ALERT "file system type : %lx\n", sym_addr);
-	printk(KERN_ALERT "file system type : %s\n", fst->name);
-
-	if (fst->next == NULL)
-		printk(KERN_ALERT "NULL\n");
-	while (fst->next != NULL){
-		printk(KERN_ALERT "file system type : %s\n", fst->name);
-		fst = fst->next;	
-	}
-	
-	 return 0;
+	return 0;
 }
 
 static void __exit list_all_file_system_type_exit(void)
